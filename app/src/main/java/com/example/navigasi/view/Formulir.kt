cab12ee0db7aa2b8e761985125d40cf0997db262
dutabrahmana <dutalinggau25@gmail.com>
2025-11-07 09:46:05 +0700

    Menambahkan fungsi composable 'FormIsian'. Ini berfungsi sebagai wadah utama formulir dan mendefinisikan parameter 'jenisK' (default Laki-Laki, Perempuan) serta callback 'OnSubmitBtnClick' untuk menangani aksi tombol kirim.

--- app/src/main/java/com/example/navigasi/view/Formulir.kt
+++ app/src/main/java/com/example/navigasi/view/Formulir.kt
@@ -1,2 +1,12 @@
 package com.example.navigasi.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+    jenisK:List<String> = listOf("Laki-Laki","Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+) {

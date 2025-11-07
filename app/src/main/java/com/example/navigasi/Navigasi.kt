c472f344ac5f10134044b655ac271925747e8449
dutabrahmana <dutalinggau25@gmail.com>
2025-11-07 13:25:23 +0700

    membuat enum class

--- app/src/main/java/com/example/navigasi/Navigasi.kt
+++ app/src/main/java/com/example/navigasi/Navigasi.kt
@@ -1,2 +1,20 @@
 package com.example.navigasi
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigasi.view.FormIsian
+import com.example.navigasi.view.TampilData
+
+
+enum class Navigation {
+    FormulirKu,
+    Detail
+}
+
+}

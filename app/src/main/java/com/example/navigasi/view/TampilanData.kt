a7015b79c3f06653f9ea6a15f2ef1175ae668a8c
dutabrahmana <dutalinggau25@gmail.com>
2025-11-07 13:34:14 +0700

    Menambahkan Button composable untuk aksi kembali. Button diatur dengan fillMaxWidth dan terhubung ke callback onBackBtnClick, serta menggunakan string resource 'back' sebagai label.

--- app/src/main/java/com/example/navigasi/view/TampilanData.kt
+++ app/src/main/java/com/example/navigasi/view/TampilanData.kt
@@ -89,4 +89,13 @@ fun TampilData(
                 }
             }
 
+            Spacer(modifier = Modifier.height(10.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = onBackBtnClick
+            ) {
+                Text(text = stringResource(id = R.string.back))
+            }
+        }
+    }
 }
